61eb3ac3cd69a6c55b22597eabdc8e069136952e
AlexCoilaJrt <alex.coila.jrt@gmail.com>
2022-04-28 09:08:24 -0500

    mi primer trabajo

---
+++ Ejercicios1Java.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Ejercicios1Java {
+    public static void main(String[] args) {
+        //2.6
+        //Definir variables
+        double a,b,c;
+        //Datos de entrada
+        Scanner lt=new Scanner(System.in);
+        System.out.println("Ingrese el lado a");
+        a=lt.nextDouble();
+        System.out.println("Ingrese el lado b");
+        a=lt.nextDouble();
+        //Proceso
+        c=Math.sqrt(Math.pow(a,  2)+Math.pow(b, 2));
+
+
+
+
+       
+
+
+    }
+}
