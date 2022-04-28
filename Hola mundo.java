30ddf077d69a6e39c37ea4648a67fcaa1908f2c7
AlexCoilaJrt <alex.coila.jrt@gmail.com>
2022-04-28 09:25:27 -0500

    mi primer trabajo

---
+++ Hola mundo.java
@@ -0,0 +1,16 @@
+public class HolaMundo{
+public static void main(String[]alex){
+//Variables y Datos de Entrada 
+int lA=4;
+int lB=5;
+int lC=3;
+//Proceso 
+int v=lA*lB*lC;
+//Datos de Salida
+System.our.println("El volumen es: "+v);
+
+
+}
+
+
+} 
